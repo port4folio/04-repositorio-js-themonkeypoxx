c12393bf016e0fedede479f7e71ac98a4525d725
Laboratorios Chillan <chilab@inacap.cl>
2026-05-11 10:51:52 -0400

    Boton agregar

--- js/lista_tareas.js
+++ js/lista_tareas.js
@@ -1,14 +1,23 @@
-let tareas=["Tarea 1", "Tarea 2"];
+let tareas=[];
 let listaTareas=document.getElementById("listaTareas");
-//console.log(listatareas)
-listarTareas(tareas)
+//console.log(listatareas);
+listarTareas(tareas);
 
 function listarTareas(t){
-    listarTareas.innerHTML="";
-    t.array.forEach(Tarea => {
+    listaTareas.innerHTML="";
+    t.forEach(tarea => {
         li=document.createElement("li");
         li.textContent=tarea;
-        listaTareas.appendChild("li")
+        listaTareas.appendChild(li);
         li.className="list-group-item";
     });
+}
+
+let btnAgregar = document.getElementById("btnAgregar"); //capura el botón
+btnAgregar.addEventListener("click", agregarTarea);  //agrega un evento click al botón agregar q activa la función agregar tarea
+//función q agrega tareas
+function agregarTarea() {
+    let tarea = document.getElementById("txtTarea").value; //captura el valor del input
+    tareas.push(tarea);  //agrega la tarea al array
+    listarTareas(tareas); //llama función para mostrar tareas en ul
 }

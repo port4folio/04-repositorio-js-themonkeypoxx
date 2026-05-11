2752b48e056fdeb03f41b84e238f70555607ceb6
Laboratorios Chillan <chilab@inacap.cl>
2026-05-11 11:31:49 -0400

    avance

--- js/lista_tareas.js
+++ js/lista_tareas.js
@@ -20,4 +20,24 @@ function agregarTarea() {
     let tarea = document.getElementById("txtTarea").value; //captura el valor del input
     tareas.push(tarea);  //agrega la tarea al array
     listarTareas(tareas); //llama función para mostrar tareas en ul
+    let btnBuscar = document.getElementById("btnBuscar");
+    btnBuscar.addEventListener("click", buscarTarea); 
+    function buscarTarea() {
+        let tareaBuscada = document.getElementById("txtTarea").value; //captura input
+        if (tareaBuscada == "") { //si input está vacío
+            listarTareas(tareas);
+        }else { //si no está vacío
+        tareasEncontradas = tareas.filter((tarea) => tarea == tareaBuscada); //filtra q concidan cn lo q buscaron lol
+        if (tareasEncontradas.length > 0){ //si se encuentra algo
+            listarTareas(tareasEncontradas);
+        } else { //si no se encuentra nada
+            Swal.fire({
+                icon: "error", //tipo msj
+                title: "Oops...", //título msj
+                text: "No se encontró NADOTA!!!", //txt del msj
+                footer: "", //pie del msj
+            });
+        }
+    }
+}
 }
